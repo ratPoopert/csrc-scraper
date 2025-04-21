a09c3376736ffc6228b3251edd1b4e0c5d55b5e4
Patrick Ruppert <patrick.j.ruppert@gmail.com>
2025-04-21 10:03:37 -0400

    Even more refactoring

--- csrc_scraper/cmvp_certificate_scraper.py
+++ csrc_scraper/cmvp_certificate_scraper.py
@@ -24,6 +24,10 @@ class CMVPCertificateScraper(Scraper):
             panel.scrape()
             self.data[panel.heading] = panel.data
 
+    @staticmethod
+    def get_absolute_url(relative_url: str) -> str:
+        return urls.join(urls.CMVP_CERTIFICATE_BASE_URL, relative_url)
+
 
 class Panel:
 
@@ -75,7 +79,9 @@ class DetailsPanel(Panel):
             case "Overall Level":
                 data = int(content.text.strip())
             case "Approved Algorithms":
-                data = _scrape_approved_algorithms_table(content)
+                table = ApprovedAlgorithmsTable(content)
+                table.scrape()
+                data = table.data
             case ("Security Level Exceptions" |
                   "Tested Configuration(s)"):
                 data = list(map(helpers.remove_newlines,
@@ -83,17 +89,6 @@ class DetailsPanel(Panel):
             case _:
                 data = helpers.condense_inner_whitespace(content.text)
         self.data[label] = data
-
-    def scrape_approved_algorithms(self, content: BeautifulSoup):
-        try:
-            tbody = content.find('tbody')
-            rows = tbody.find_all('tr')
-            entries = map(_scrape_approved_algorithm_table_row, rows)
-            return list(filter(lambda e: e[0], entries))
-        except AttributeError:
-            rows = content.find_all('div', class_="row", recursive=False)
-            entries = map(_scrape_approved_algorithm_div_row, rows)
-            return list(filter(lambda e: e[0], entries))
 
 
 class VendorPanel(Panel):
@@ -126,7 +121,31 @@ class VendorPanel(Panel):
         self.data["Website"] = website
 
     def scrape_contacts(self):
-        self.data["Contacts"] = _scrape_vendor_contacts(self.body.find('div'))
+        container = self.body.find('div')
+        contacts = container.find_all('span', recursive=False)
+        self.data["Contacts"] = list(map(
+            self.scrape_contact,
+            contacts
+        ))
+
+    @staticmethod
+    def scrape_contact(contact: BeautifulSoup):
+        strings = contact.stripped_strings
+        result = {
+            "Name": next(strings),
+            "Email": next(strings)
+        }
+
+        phone_str = next(strings, '')
+        phone = phone_str.lstrip("Phone: ") if "Phone: " in phone_str else None
+        fax_str = next(strings, '')
+        fax = fax_str.lstrip("Fax: ") if "Fax: " in fax_str else None
+
+        if phone:
+            result["Phone"] = phone
+        if fax:
+            result["Fax"] = fax
+        return result
 
 
 class RelatedFilesPanel(Panel):
@@ -136,7 +155,8 @@ class RelatedFilesPanel(Panel):
     def scrape(self):
         links = self.body.find_all('a')
         def label(link): return link.text.strip()
-        def url(link): return _resolve_absolute_url(link.get('href'))
+        def url(link): return (CMVPCertificateScraper
+                               .get_absolute_url(link.get('href')))
         self.data = [[label(link), url(link)] for link in links]
 
 
@@ -158,95 +178,38 @@ class ValidationHistoryPanel(Panel):
         })
 
 
-def _scrape_approved_algorithms_table(soup: BeautifulSoup):
-    try:
-        tbody = soup.find('tbody')
-        rows = tbody.find_all('tr')
-        entries = map(_scrape_approved_algorithm_table_row, rows)
-        return list(filter(lambda e: e[0], entries))
-    except AttributeError:
-        rows = soup.find_all('div', class_="row", recursive=False)
-        entries = map(_scrape_approved_algorithm_div_row, rows)
-        return list(filter(lambda e: e[0], entries))
+class ApprovedAlgorithmsTable:
 
+    def __init__(self, soup: BeautifulSoup):
+        self.soup = soup
+        self.data = []
 
-def _scrape_approved_algorithm_table_row(soup: BeautifulSoup):
-    alg, val = soup.find_all('td', recursive=False)
-    return [
-        alg.text.strip(),
-        val.text.strip(),
-        [_resolve_absolute_url(a.get('href')) for a in val.find_all('a')]
-    ]
+    def scrape(self):
+        try:
+            tbody = self.soup.find('tbody')
+            rows = tbody.find_all('tr')
+            entries = map(self.scrape_table_row, rows)
+            self.data = list(filter(lambda e: e[0], entries))
+        except AttributeError:
+            rows = self.soup.find_all('div', class_="row", recursive=False)
+            entries = map(self.scrape_div_row, rows)
+            self.data = list(filter(lambda e: e[0], entries))
 
+    def scrape_table_row(self, row: BeautifulSoup):
+        alg, val = row.find_all('td', recursive=False)
+        return [
+            alg.text.strip(),
+            val.text.strip(),
+            [CMVPCertificateScraper.get_absolute_url(a.get('href'))
+             for a in val.find_all('a')]
+        ]
 
-def _scrape_approved_algorithm_div_row(soup: BeautifulSoup):
-    alg = soup.find('div', class_="col-md-3")
-    val = soup.find('div', class_="col-md-4")
-    return [
-        alg.text.strip(),
-        val.text.strip(),
-        [_resolve_absolute_url(a.get('href')) for a in val.find_all('a')]
-    ]
-
-
-def _scrape_vendor_panel(soup: BeautifulSoup) -> dict:
-    try:
-        name = soup.find('a').text.strip()
-        website = soup.find('a').get('href', '')
-    except AttributeError:
-        name = next(soup.stripped_strings)
-        website = ""
-    return {
-        "Name": name,
-        "Website": website,
-        "Address": "\n".join(map(lambda el: el.text.strip(),
-                                 soup.find_all('span', recursive=False))),
-        "Contacts": _scrape_vendor_contacts(soup.find('div'))
-    }
-
-
-def _scrape_vendor_contacts(soup: BeautifulSoup) -> list[dict]:
-    return list(map(_scrape_vendor_contact,
-                    soup.find_all('span', recursive=False)))
-
-
-def _scrape_vendor_contact(soup: BeautifulSoup) -> dict:
-    strings = soup.stripped_strings
-    result = {
-        "Name": next(strings),
-        "Email": next(strings)
-    }
-
-    phone_str = next(strings, '')
-    phone = phone_str.lstrip("Phone: ") if "Phone: " in phone_str else None
-    fax_str = next(strings, '')
-    fax = fax_str.lstrip("Fax: ") if "Fax: " in fax_str else None
-
-    if phone:
-        result["Phone"] = phone
-    if fax:
-        result["Fax"] = fax
-    return result
-
-
-def _scrape_related_files_panel(soup: BeautifulSoup) -> list:
-    return [[el.text.strip(), _resolve_absolute_url(el.get('href'))]
-            for el in soup.find_all('a')]
-
-
-def _scrape_validation_history_panel(soup: BeautifulSoup) -> list[dict]:
-    return list(map(_scrape_validation_history_table_row,
-                    soup.find('tbody').find_all('tr')))
-
-
-def _scrape_validation_history_table_row(soup: BeautifulSoup) -> dict:
-    cells = soup.find_all('td')
-    return {
-        "Date": cells[0].text.strip(),
-        "Type": cells[1].text.strip(),
-        "Lab": cells[2].text.strip(),
-    }
-
-
-def _resolve_absolute_url(relative_url: str) -> str:
-    return urls.join(urls.CMVP_CERTIFICATE_BASE_URL, relative_url)
+    def scrape_div_row(self, row: BeautifulSoup):
+        alg = row.find('div', class_="col-md-3")
+        val = row.find('div', class_="col-md-4")
+        return [
+            alg.text.strip(),
+            val.text.strip(),
+            [CMVPCertificateScraper.get_absolute_url(a.get('href'))
+             for a in val.find_all('a')]
+        ]
